e458b67705a878a240ea91c2373b05f3c3b6856f
dosjcq <nagibin.artyom1999@gmail.com>
2025-02-24 20:55:42 -0500

    fix: add debugging

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // Если index.html лежит в корне проекта
+    },
+  },
 })
